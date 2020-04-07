55b933c175dcb15870bd7b8f9ce38c66da059df1
Alexander Smyslov <smyslov@selby.su>
2020-04-07 22:10:19 +0300

    v200407b: HtmlDomParser lib connected to Tag Parser tool

--- tools/core/selby/TagParser.php
+++ tools/core/selby/TagParser.php
@@ -2,11 +2,14 @@
 
 namespace tools\core\selby;
 
+use PHPHtmlParser\Dom;
+
 class TagParser extends BaseTool
 {
     public function work()
     {
         $result = '';
+        $dom = new Dom;
 
         for ($i = 0; $i < min(count($this->input), TOOLS_ROWS_LIMIT); $i++) {
 
@@ -18,33 +21,29 @@ class TagParser extends BaseTool
 
             if (filter_var($url, FILTER_VALIDATE_URL)) {
 
-                $page = @file_get_contents(trim($this->input[$i]));
+                $dom->load($url);
 
                 switch ($this->options['tag']) {
                     case 'title':
-                        $pattern = '#<title>(.+?)</title>#su';
-                        preg_match($pattern, $page, $match);
+                        $match = strip_tags($dom->find('title')[0]->innerHtml);
                         break;
 
                     case 'h1':
-                        $pattern = '#<h1[^>]*?>(.+?)</h1>#su';
-                        preg_match($pattern, $page, $match);
+                        $match = strip_tags($dom->find('h1')[0]->innerHtml);
                         break;
 
                     case 'description':
-                        $pattern = '#<meta(?=[^>]* name *= *"?description"?) [^>]*?(?<= )content *= *"([^"]*)"[^>]*>#su';
-                        preg_match($pattern, $page, $match);
+                        $match = strip_tags($dom->find('description')[0]->innerHtml);
                         break;
 
                     case 'keywords':
-                        $pattern = '#<meta(?=[^>]* name *= *"?keywords"?) [^>]*?(?<= )content *= *"([^"]*)"[^>]*>#su';
-                        preg_match($pattern, $page, $match);
+                        $match = strip_tags($dom->find('keywords')[0]->innerHtml);
                         break;
                     
                     default:
-                        $match = [];
+                        $match = 'error';
                 }
-                $result .= $match[1] ?: 'error';
+                $result .= $match;
 
             } else {
                 $result .= 'error';
@@ -53,6 +52,6 @@ class TagParser extends BaseTool
             $result .= PHP_EOL;
         }
 
-        return trim($result);
+        return $result;
     }
 }
